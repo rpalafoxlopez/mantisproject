b713455ae2de0bc0502635246f912604706dc6dd
rpalafox <raf4@live.com.mx>
2026-05-26 16:13:21 -0600

    Update frontend config

--- apps/frontend/src/router/index.js
+++ apps/frontend/src/router/index.js
@@ -4,34 +4,34 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('./views/HomeView.vue')
+    component: () => import('@/views/HomeView.vue')
   },
   {
     path: '/admin',
     name: 'Admin',
-    component: () => import('./views/AdminView.vue')
+    component: () => import('@/views/AdminView.vue')
   },
   {
     path: '/host/:sessionCode?',
     name: 'Host',
-    component: () => import('./views/HostView.vue'),
+    component: () => import('@/views/HostView.vue'),
     props: true
   },
   {
     path: '/join',
     name: 'Join',
-    component: () => import('./views/JoinView.vue')
+    component: () => import('@/views/JoinView.vue')
   },
   {
     path: '/play/:sessionCode',
     name: 'Play',
-    component: () => import('./views/PlayView.vue'),
+    component: () => import('@/views/PlayView.vue'),
     props: true
   },
   {
     path: '/results/:sessionCode',
     name: 'Results',
-    component: () => import('./views/ResultsView.vue'),
+    component: () => import('@/views/ResultsView.vue'),
     props: true
   }
 ]
