c385b2fe103d570e9d4735c7bb6ae4d2bb46e8a5
rpalafox <raf4@live.com.mx>
2026-05-28 15:58:36 -0600

    Update backend and frontend

--- apps/frontend/src/views/HostView.vue
+++ apps/frontend/src/views/HostView.vue
@@ -2,7 +2,7 @@
   <div class="host-container">
     <header class="host-header">
       <div class="logo-section">
-        <img src="./img/logo.png" alt="MANTIS" class="host-logo" />
+        <img src="/img/quizhive.png" alt="MANTIS" class="host-logo" />
       </div>
       <div class="header-actions">
         <button class="btn-exit" @click="exitGame">
